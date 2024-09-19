d6936f89e333d6ebddb0c3bfd68e4eb992b29dbd
Guilherme9639 <cardozoalmeidaguilherme@gmail.com>
2024-09-19 09:36:20 -0300

    Primeiro exercicio de condicional

--- Faculdade.py
+++ Faculdade.py
@@ -1,1 +1,12 @@
 #Estruturas Condicionais com Python#
+
+idade = int(input("Coloque aqui a sua idade: "))
+
+if idade < 18:
+  print("Menor de idade")
+
+elif idade > 18 and idade < 65:
+  print("Adulto")
+
+else:
+  print("Idoso")
