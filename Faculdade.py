e465b50ab44700cdcb74a2eb4ad4c65d7bacf2f8
Guilherme9639 <cardozoalmeidaguilherme@gmail.com>
2024-09-19 09:48:01 -0300

    exercicio de fixação

--- Faculdade.py
+++ Faculdade.py
@@ -10,3 +10,17 @@ elif idade >= 18 and idade < 65:
 
 else:
   print("Idoso")
+
+
+#Exercicio de fixação#
+
+idade = int(input("Coloque aqui a sua idade: "))
+
+if idade < 12:
+  print("Recomendamos o filme intantil Filme1")
+
+elif 12 <= idade < 18:
+  print("Recomendamos o filme2")
+
+else:
+  print("Recomendamos filme 3")
