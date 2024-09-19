d1071364758bf3651660c6af327795380c2a4ba0
Guilherme9639 <cardozoalmeidaguilherme@gmail.com>
2024-09-19 09:38:21 -0300

    faltou colocar um sinal de = na idade 18

--- Faculdade.py
+++ Faculdade.py
@@ -5,7 +5,7 @@ idade = int(input("Coloque aqui a sua idade: "))
 if idade < 18:
   print("Menor de idade")
 
-elif idade > 18 and idade < 65:
+elif idade >= 18 and idade < 65:
   print("Adulto")
 
 else:
